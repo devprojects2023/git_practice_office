ab8190fc7a0d2070215fb443e06e8f257f17d572
devprojects2023 <devpay2022@gmail.com>
2024-04-29 11:59:12 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,11 @@
+name = ["Dev", "Avik", "Raj"]
+def myname():
+    if user_input in name:
+        print("Available")
+    else:
+        name.append(user_input)
+        print(f"{user_input} is added in the list")
+        print(name)
+
+user_input = input("Enter your name: \n")
+myname()
